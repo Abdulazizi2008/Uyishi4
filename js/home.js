0932d7b8415124b49a7711c8d74ad0efc536bbbd
Abdulazizi2008 <ozodbekovabdu@gmail.com>
2023-12-07 15:03:02 +0500

    yees

--- js/home.js
+++ js/home.js
@@ -9,3 +9,14 @@ btnOpen.addEventListener("click", function (e) {
 btnClose.addEventListener("click", function (e) {
   modal.classList.remove("open");
 });
+const header = document.querySelector("header");
+const menuBtn = document.querySelector(".menu");
+
+menuBtn.addEventListener("click", () => {
+  header.classList.toggle("open");
+});
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+});
